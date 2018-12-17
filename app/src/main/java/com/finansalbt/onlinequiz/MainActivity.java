8b9049053c78de8871646ac8ae89aa19f3aa5940
sermetoztoprak <sermetoztoprak@gmail.com>
2018-12-17 21:22:44 +0300

    Sign in Sign up

---
+++ app/src/main/java/com/finansalbt/onlinequiz/MainActivity.java
@@ -0,0 +1,146 @@
+package com.finansalbt.onlinequiz;
+
+import android.content.DialogInterface;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.finansalbt.onlinequiz.Model.User;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.rengwuxian.materialedittext.MaterialEditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    MaterialEditText edtNewUser,edtNewPassword,edtNewEmail;//Sign Up
+    MaterialEditText edtUser,edtPassword;//Sign In
+
+    Button btnSignUp,btnSignIn;
+
+    FirebaseDatabase database;
+    DatabaseReference users;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        database = FirebaseDatabase.getInstance();
+        users = database.getReference("Users");
+
+        edtUser = findViewById(R.id.edtUser);
+        edtPassword = findViewById(R.id.edtPassword);
+
+        btnSignIn=findViewById(R.id.btn_sign_in);
+        btnSignUp=findViewById(R.id.btn_sign_up);
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showSignUpDialog();
+            }
+        });
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signIn(edtUser.getText().toString(),edtPassword.getText().toString());
+            }
+        });
+
+    }
+    private void signIn(final String user, final String pwd) {
+        users.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.child(user).exists())
+                {
+                    if (!user.isEmpty())
+                    {
+                        User login = dataSnapshot.child(user).getValue(User.class);
+                        if (login.getPassword().equals(pwd))
+                            Toast.makeText(MainActivity.this, "Login ok !", Toast.LENGTH_SHORT).show();
+                        else
+                            Toast.makeText(MainActivity.this, "Wrong password", Toast.LENGTH_SHORT).show();
+                    }
+                    else{
+                        Toast.makeText(MainActivity.this, "Please enter your user name", Toast.LENGTH_SHORT).show();
+                    }
+                }
+                else
+                {
+                    Toast.makeText(MainActivity.this, "User is not exists !", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+
+    private void showSignUpDialog() {
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(MainActivity.this);
+        alertDialog.setTitle("Sign Up");
+        alertDialog.setMessage("Please fill full information");
+
+        LayoutInflater inflater = this.getLayoutInflater();
+        View sign_up_layout = inflater.inflate(R.layout.sign_up_layout,null);
+
+        edtNewUser = sign_up_layout.findViewById(R.id.edtNewUserName);
+        edtNewEmail = sign_up_layout.findViewById(R.id.edtNewEmail);
+        edtNewPassword = sign_up_layout.findViewById(R.id.edtNewPassword);
+
+        alertDialog.setView(sign_up_layout);
+        alertDialog.setIcon(R.drawable.ic_account_circle_black_24dp);
+
+        alertDialog.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int which) {
+                dialogInterface.dismiss();
+            }
+        });
+
+        alertDialog.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int which) {
+                final User user = new User(edtNewUser.getText().toString(),
+                        edtNewPassword.getText().toString(),
+                        edtNewEmail.getText().toString());
+
+
+                users.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        if(dataSnapshot.child(user.getUserName()).exists())
+                            Toast.makeText(MainActivity.this, "User already exists ! ", Toast.LENGTH_SHORT).show();
+                        else {
+                            users.child(user.getUserName()).setValue(user);
+                            Toast.makeText(MainActivity.this, "User registration success !", Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+                dialogInterface.dismiss();
+            }
+        });
+
+        alertDialog.show();
+
+    }
+
+}
